f680da139588b9ecd372ec903b067ec6d3b4d2be
Douglas Snoddy Jr <67167826+DSnod93@users.noreply.github.com>
2023-06-26 21:28:49 -0700

    Added searchbox functionality

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { kitten } from "./kitten";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      kitten: kitten,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const filteredKittens = this.state.kitten.filter((kitten) => {
+        return kitten.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+      });
+    return (
+      <div className="tc">
+        <h1>KittenFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList kitten={filteredKittens} />
+      </div>
+    );
+  }
+}
+
+export default App;
