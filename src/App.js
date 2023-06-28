e0b1fb51ae4ad3e45885a5f39360d0f601d5ab99
Douglas Snoddy Jr <67167826+DSnod93@users.noreply.github.com>
2023-06-27 19:42:38 -0700

    Fetch response from url

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { kitten } from "./kitten";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      kitten: kitten,
+      kitten: [],
       searchfield: "",
     };
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ kitten: users }));
   }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
   render() {
     const filteredKittens = this.state.kitten.filter((kitten) => {
-        return kitten.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-      });
-    return (
-      <div className="tc">
-        <h1 className="f1">KittenFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList kitten={filteredKittens} />
-      </div>
-    );
+      return kitten.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    if (this.state.kitten.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">KittenFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList kitten={filteredKittens} />
+        </div>
+      );
+    }
   }
 }
 
